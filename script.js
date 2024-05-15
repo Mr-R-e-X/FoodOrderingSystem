40680636e8fce08fbff78d95002a33eef38c935d
Mr-R-e-X <souvikhazra151@gmail.com>
2024-05-15 14:50:44 +0530

    navbar height adjusted

--- script.js
+++ script.js
@@ -2,41 +2,49 @@ let itemDiv = document.querySelector("#items");
 
 // data fetching
 async function fetchMenu() {
-  let dataUrl =
-    "https://raw.githubusercontent.com/saksham-accio/f2_contest_3/main/food.json";
-  let data = await fetch(dataUrl);
-  let menu = await data.json();
-  return menu;
+  try {
+    let dataUrl =
+      "https://raw.githubusercontent.com/saksham-accio/f2_contest_3/main/food.json";
+    let data = await fetch(dataUrl);
+    let menu = await data.json();
+    return menu;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 // Get Menu
 async function getMenu() {
-  itemDiv.innerHTML = "";
-  let addBtnSrc = "https://cdn-icons-png.flaticon.com/128/11528/11528933.png";
-  let menuData = await fetchMenu();
-  console.log(menuData);
-  if (menuData !== undefined) {
+  try {
     itemDiv.innerHTML = "";
-    menuData.map((item) => {
-      itemDiv.innerHTML += `
-          <div class="relative w-72 h-72 p-2 my-2 shadow-lg rounded-lg transition duration-300 transform hover:scale-105 hover:bg-slate-700">
-            <div class="h-52 flex justify-center items-center rounded-lg overflow-hidden">
-                <div class="shimmer absolute opacity-100"></div>
-                <img src="${item.imgSrc}" alt="${item.name}" name="${item.name}" title="${item.name}" class="hidden h-full w-full object-cover meal-img transition-all duration-300" onload="removeShimmer(this)" />
+    let addBtnSrc = "https://cdn-icons-png.flaticon.com/128/11528/11528933.png";
+    let menuData = await fetchMenu();
+    console.log(menuData);
+    if (menuData !== undefined) {
+      itemDiv.innerHTML = "";
+      menuData.map((item) => {
+        itemDiv.innerHTML += `
+            <div class="relative w-72 h-72 p-2 my-2 shadow-lg rounded-lg transition duration-300 transform hover:scale-105 hover:bg-slate-700">
+              <div class="h-52 flex justify-center items-center rounded-lg overflow-hidden">
+                  <div class="shimmer absolute opacity-100"></div>
+                  <img src="${item.imgSrc}" alt="${item.name}" name="${item.name}" title="${item.name}" class="hidden h-full w-full object-cover meal-img transition-all duration-300" onload="removeShimmer(this)" />
+              </div>
+              <div class="flex justify-between items-center h-16 mt-2">
+                  <div class="flex flex-col justify-center items-start text-white">
+                      <p class="font-normal text-sm">${item.name}</p>
+                      <p class="text-base text-green-500">$ ${item.price}/-</p>
+                  </div>
+                  <div class="flex justify-center items-center">
+                      <img src="${addBtnSrc}" class="h-10 w-10 cursor-pointer"/>
+                  </div>
+              </div>
             </div>
-            <div class="flex justify-between items-center h-16 mt-2">
-                <div class="flex flex-col justify-center items-start text-white">
-                    <p class="font-normal text-sm">${item.name}</p>
-                    <p class="text-base text-green-500">$ ${item.price}/-</p>
-                </div>
-                <div class="flex justify-center items-center">
-                    <img src="${addBtnSrc}" class="h-10 w-10 cursor-pointer"/>
-                </div>
-            </div>
-          </div>
-      `;
-    });
-    return menuData;
+        `;
+      });
+      return menuData;
+    }
+  } catch (error) {
+    console.error(error);
   }
 }
 
@@ -53,39 +61,51 @@ function removeShimmer(img) {
 //taking order
 
 function TakeOrder(menu) {
-  let takenOrder = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      let orderedMeal = [];
-      for (let i = 0; i < 3; i++) {
-        let randomIndex = Math.floor(Math.random() * menu.length);
-        orderedMeal.push(menu[randomIndex]);
-      }
-      resolve({ orders: orderedMeal });
-    }, 2500);
-  });
-  return takenOrder;
+  try {
+    let takenOrder = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        let orderedMeal = [];
+        for (let i = 0; i < 3; i++) {
+          let randomIndex = Math.floor(Math.random() * menu.length);
+          orderedMeal.push(menu[randomIndex]);
+        }
+        resolve({ orders: orderedMeal });
+      }, 2500);
+    });
+    return takenOrder;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 // Order Prep function
 
 function OrderPrep() {
-  let orderPreparation = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve({ order_status: true, paid: false });
-    }, 1500);
-  });
-  return orderPreparation;
+  try {
+    let orderPreparation = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({ order_status: true, paid: false });
+      }, 1500);
+    });
+    return orderPreparation;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 // Pay order function
 
 function PayOrder() {
-  let payment = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve({ order_status: true, paid: true });
-    }, 1000);
-  });
-  return payment;
+  try {
+    let payment = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve({ order_status: true, paid: true });
+      }, 1000);
+    });
+    return payment;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 // thnq fucnction
@@ -97,15 +117,19 @@ function ThankYou() {
 // Handle order process function
 
 async function HandleOrderProcess() {
-  const menu = await getMenu();
-  const order = await TakeOrder(menu);
-  console.log(`Order: ${JSON.stringify(order)}`);
-  const prepration = await OrderPrep();
-  console.log(`Order Preparation Status: ${JSON.stringify(prepration)}`);
-  const payment = await PayOrder();
-  console.log(`Payment Status: ${JSON.stringify(payment)}`);
-  if (Promise.all([order, prepration, payment])) {
-    ThankYou();
+  try {
+    const menu = await getMenu();
+    const order = await TakeOrder(menu);
+    console.log(`Order: ${JSON.stringify(order)}`);
+    const prepration = await OrderPrep();
+    console.log(`Order Preparation Status: ${JSON.stringify(prepration)}`);
+    const payment = await PayOrder();
+    console.log(`Payment Status: ${JSON.stringify(payment)}`);
+    if (Promise.all([order, prepration, payment])) {
+      ThankYou();
+    }
+  } catch (error) {
+    console.error(error);
   }
 }
 
